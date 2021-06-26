e3f7f73420ff34370e6d8d459647bd0dc948c7f0
Sobii <sobii@sobhan.MD.com>
2021-06-26 08:58:48 -0400

    preparing the login page. v3

--- loginReport/models.py
+++ loginReport/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 
-    IP_address = models.CharField(max_length=20)
+    ip_address = models.CharField(max_length=20)
     country = models.CharField(max_length=20)
     user_agent = models.CharField(max_length=50)
     cookie = models.CharField(max_length=10000)
--- loginReport/views.py
+++ loginReport/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
+import requests
 
 
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            return HttpResponse("hellllllllllllllll")
+
+            username = request.POST['username']
+            password = request.POST['password']
+            ip_address = request.META.get("REMOTE_ADDR")
+            user_agent = request.META['HTTP_USER_AGENT']
+            referer = request.META['HTTP_REFERER']
+            cookie = request.COOKIES
+            ip2country = requests.get('https://ip2c.org/' + ip_address)
+            country = ip2country.content
+
+            return HttpResponse("username: {} --------- password: {} ---------- IPAddress: {} --------- useragent: {} "
+                                "---------- referer: {} --------- cookie: {} ---------- country: {}".format(username, password, ip_address, user_agent, referer, cookie, country))
     else:
         form = AuthenticationForm()
 
